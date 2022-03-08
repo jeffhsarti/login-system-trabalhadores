32435b632fef8bae30fd0f94c5791d5f7dd6d994
Jefferson <jeffersonsarti@gmail.com>
2022-03-08 00:40:56 -0300

    feat: express config and initialization

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const figlet = require('figlet');
+const config = require('./config/app');
+
+const app = express();
+app.use(express.json());
+app.use(cors()); // adicionar corsOptions
+
+const controllers = require("./src/controllers");
+Object.keys(controllers).forEach(controllerName => {
+  controllers[controllerName](app);
+})
+
+app.listen(config.port || 8080, () => {
+  figlet.text(
+    "FIAP - Trabalhadores",
+    {
+      font: "Standard",
+      horizontalLayout: "fitted",
+      verticalLayout: "fitted",
+      width: 200,
+      whitespaceBreak: true,
+    },
+    function (err, data) {
+      if (err) {
+        console.log("FIAP - Trabalhadores");
+        return;
+      }
+      console.log(data);
+      console.log("\n\n");
+      console.log(`Listening on port ${config.port}`)
+    }
+  );
+});
