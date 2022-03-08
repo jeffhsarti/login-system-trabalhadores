2cea59e5dd7121fb10b77bb903ef07cb6981934e
Jefferson <jeffersonsarti@gmail.com>
2022-03-08 02:22:56 -0300

    refactor: changed secret token getter

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,13 +1,15 @@
 const jwt = require("jsonwebtoken");
+const config = require("../../config/app");
 
 async function authValidation(req, res, next) {
-  const auth = req.headers["Authorization"];
+  const auth = req.headers.authorization;
   if (!auth) {
     return res.status(401).json({ error: "Token inválido." });
   }
   const token = auth.split(" ")[1];
-  jwt.verify(token, process.env.JWT_SECRET, function (err) {
+  jwt.verify(token, config.secret, function (err) {
     if (err) {
+      console.log(err);
       return res.status(401).json({ error: "Token inválido." });
     }
     return next();
